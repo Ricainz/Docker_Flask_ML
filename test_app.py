5adb5ff5efdf7142dfa39eff72f988db3340749f
Ricainz <clem.lambling@hotmail.fr>
2020-11-09 14:21:46 +0100

    added tests

--- test_app.py
+++ test_app.py
@@ -1,12 +1,43 @@
 import unittest
 import requests
+import json
+from flask import jsonify 
 
 
 class TestApp(unittest.TestCase):
 
     def test_a_base(self):
-        responce = requests.get('http://localhost:5000')
-        self.assertEqual(responce.status_code, 200)
+        response = requests.get('http://localhost:5000')
+        self.assertEqual(response.status_code, 200)
 
-   # def test_result(self):
+    def test_result_neutral(self):
+
+        params = {
+            'Sentence': 'ok'
+        }
+        response = requests.post('http://localhost:5000/get_result', json=params)
+        print(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text.strip().replace('"', ''), "neutral")
+
+    def test_result_positive(self):
+
+        params = {
+            'Sentence': 'nice'
+        }
+        response = requests.post('http://localhost:5000/get_result', json=params)
+        print(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text.strip().replace('"', ''), "positive")
+
+    def test_result_negative(self):
+
+        params = {
+            'Sentence': 'worst'
+        }
+        response = requests.post('http://localhost:5000/get_result', json=params)
+        print(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text.strip().replace('"', ''), "negative")
+
 
